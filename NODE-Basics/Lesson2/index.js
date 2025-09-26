afd501dcd13bc9ce0232fbb84909fa523e508b9a
Debajit91 <debajitroy544@gmail.com>
2025-09-26 21:55:44 +0600

    read a file using fs method

--- NODE-Basics/Lesson2/index.js
+++ NODE-Basics/Lesson2/index.js
@@ -2,19 +2,28 @@
 const fs = require('fs');
 
 // new file create / write / override
-fs.writeFile('demo1.txt', 'My Name is Debajit Roy.', (err) =>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('successful')
-    }
-})
+// fs.writeFile('demo1.txt', 'My Name is Debajit Roy.', (err) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('successful')
+//     }
+// })
 
 // append a file
-fs.appendFile('demo1.txt', ' I am 30 years old.', (err) =>{
+// fs.appendFile('demo1.txt', ' I am 30 years old.', (err) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('successful')
+//     }
+// })
+
+// read a file - utf-8 encoding system should apply to read data.
+fs.readFile('demo1.txt', 'utf-8', (err, data) =>{
     if(err){
         console.log(err)
     }else{
-        console.log('successful')
+        console.log(data)
     }
 })
