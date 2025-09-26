07b1e178ba9db78d630f1ba4e9c4647e0e2970c2
Debajit91 <debajitroy544@gmail.com>
2025-09-26 21:48:37 +0600

    append file using fs method

--- NODE-Basics/Lesson2/index.js
+++ NODE-Basics/Lesson2/index.js
@@ -2,10 +2,19 @@
 const fs = require('fs');
 
 // new file create / write / override
-fs.writeFile('demo1.txt', 'My Name is Debajit Roy', function(err){
+fs.writeFile('demo1.txt', 'My Name is Debajit Roy.', (err) =>{
     if(err){
         console.log(err)
     }else{
         console.log('successful')
     }
-} )
+})
+
+// append a file
+fs.appendFile('demo1.txt', ' I am 30 years old.', (err) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('successful')
+    }
+})
