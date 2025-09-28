d8319fd3cea385136ee45ebd305773bfeac17bbd
Debajit91 <debajitroy544@gmail.com>
2025-09-28 23:47:25 +0600

    http res, req model and status code

--- NODE-Basics/Lesson4/server.js
+++ NODE-Basics/Lesson4/server.js
@@ -1,9 +1,10 @@
 const http = require('http');
-const http = require('http');
 const port = 3000;
 
 const myServer = http.createServer((req, res)=>{
-    res.end("<h1>Hello. This is the myServer </h1>");
+    res.writeHead(202, {'content-type':'text/html'})
+    res.write("<h1>Hello. This is the myServer </h1>");
+    res.end();
 });
 
 myServer.listen(port, ()=>{
