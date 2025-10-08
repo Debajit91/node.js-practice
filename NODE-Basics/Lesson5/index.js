0ea1bd2b5ede9084967337a5759c70d9d74c27e1
Debajit91 <debajitroy544@gmail.com>
2025-10-08 22:12:14 +0600

    used the get movie name package

--- NODE-Basics/Lesson5/index.js
+++ NODE-Basics/Lesson5/index.js
@@ -1,2 +1,7 @@
 const getRandomFruitsName = require('random-fruits-name')
 console.log(getRandomFruitsName('en'));
+
+
+var moviesNames = require('movies-names');
+// console.log(moviesNames.all);
+console.log(moviesNames.random(2));
