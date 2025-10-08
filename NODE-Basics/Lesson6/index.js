678028f5e2fe9b216e15baf45a83185de0a759c2
Debajit91 <debajitroy544@gmail.com>
2025-10-09 00:07:23 +0600

    http routes created

--- NODE-Basics/Lesson6/index.js
+++ NODE-Basics/Lesson6/index.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 const PORT = 3000;
 
 const app = http.createServer((req, res) =>{
-    console.log(req.url);
-    res.end("Welcome to my server");
+    if(req.url === '/'){
+        fs.readFile('index.html', (err, data)=>{
+            res.writeHead(200, {"content-type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    } else if(req.url === '/about'){
+        fs.readFile('about.html', (err, data)=>{
+            res.writeHead(200, {"content-type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    } else if(req.url === '/contact'){
+        fs.readFile('contact.html', (err, data)=>{
+            res.writeHead(200, {"content-type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    } else {
+        fs.readFile('error.html', (err, data)=>{
+            res.writeHead(404, {"content-type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
 })
 
 app.listen(PORT, ()=>{
